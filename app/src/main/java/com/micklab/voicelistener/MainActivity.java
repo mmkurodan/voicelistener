fd1282cb92fe0afb224f1d6ebcd28258aa4cdf6c
mmkurodan <mmkuroda@gmail.com>
2026-02-22 10:11:36 +0000

    Initial commit

---
+++ app/src/main/java/com/micklab/voicelistener/MainActivity.java
@@ -0,0 +1,18 @@
+package com.micklab.voicelistener;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("Hello from voicelistener!");
+        tv.setTextSize(24);
+
+        setContentView(tv);
+    }
+}
